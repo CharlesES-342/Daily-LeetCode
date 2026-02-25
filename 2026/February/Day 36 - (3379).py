d5e8634948d348c8f25fa501bae62958c93a3f78
CharlesES-342 <charlessmelt37@gmail.com>
2026-02-25 10:23:27 +0000

    updated feb namiong for easy findinf for later questions

--- 2026/February/Day 36 - (3379).py
+++ 2026/February/Day 36 - (3379).py
@@ -1,3 +1,5 @@
+#3379. Transformed Array
+
 # You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
 # For each index i (where 0 <= i < nums.length), perform the following independent actions:
 # If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
