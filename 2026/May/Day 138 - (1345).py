ca8e028bbd86d30bf04ca48ddeb2f76d3b33c84c
CharlesES-342 <charlessmelt37@gmail.com>
2026-05-18 09:57:35 +0100

    Day 138 - jumps to get to the end given some conditions

---
+++ 2026/May/Day 138 - (1345).py
@@ -0,0 +1,73 @@
+# 1345. Jump Game IV
+
+# Given an array of integers arr, you are initially positioned at the first index of the array.
+# In one step you can jump from index i to index:
+# i + 1 where: i + 1 < arr.length.
+# i - 1 where: i - 1 >= 0.
+# j where: arr[i] == arr[j] and i != j.
+# Return the minimum number of steps to reach the last index of the array.
+# Notice that you can not jump outside of the array at any time.
+
+# Example 1:
+# Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+# Output: 3
+# Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+
+# Example 2:
+# Input: arr = [7]
+# Output: 0
+# Explanation: Start index is the last index. You do not need to jump.
+
+# Example 3:
+# Input: arr = [7,6,9,6,9,6,9,7]
+# Output: 1
+# Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+
+# Constraints:
+# 1 <= arr.length <= 5 * 104
+# -108 <= arr[i] <= 108
+
+class Solution(object):
+    def minJumps(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n = len(arr)
+        if n <= 1:
+            return 0
+
+        # form a graph with edges being the valid jumps
+        graph = defaultdict(list)
+        for i, val in enumerate(arr):
+            graph[val].append(i)
+
+        # form the graph
+        
+        # navigate the graph, returning the minimum number of jumps to get to the end state 
+        queue = deque([(0, 0)])
+        visited = {0}
+        
+        while queue:
+            idx, jumps = queue.popleft()
+            
+            # last index = complete
+            if idx == n - 1:
+                return jumps
+                
+            # all potential next steps
+            next_indices = [idx - 1, idx + 1]
+            
+            # add indices with the same value
+            if arr[idx] in graph:
+                next_indices.extend(graph[arr[idx]])
+                # dont check these indicies again
+                del graph[arr[idx]]
+                
+            # process valid neighbours
+            for next_idx in next_indices:
+                if 0 <= next_idx < n and next_idx not in visited:
+                    visited.add(next_idx)
+                    queue.append((next_idx, jumps + 1))
+                    
+        return -1
